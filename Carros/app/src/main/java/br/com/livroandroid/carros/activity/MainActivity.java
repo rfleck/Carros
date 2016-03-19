a18c0805249b205caeed61198957ecab130c3c17
Roberson Fleck <fleckpower@gmail.com>
2016-03-19 14:51:40 -0300

    OK

--- Carros/app/src/main/java/br/com/livroandroid/carros/activity/MainActivity.java
+++ Carros/app/src/main/java/br/com/livroandroid/carros/activity/MainActivity.java
@@ -1,9 +1,13 @@
 package br.com.livroandroid.carros.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
 
 import br.com.livroandroid.carros.R;
+import br.com.livroandroid.carros.adapter.TabsAdapter;
 
 public class MainActivity extends BaseActivity {
 
@@ -14,5 +18,26 @@ public class MainActivity extends BaseActivity {
 
         setUpToolbar();
         setupNavDrawer();
+        setupViewPagerTabs();
+    }
+
+    // Configura o ViewPager + Tabs
+    private void setupViewPagerTabs() {
+// ViewPager
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setOffscreenPageLimit(2);
+        viewPager.setAdapter(new TabsAdapter(getContext(), getSupportFragmentManager()));
+// Tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+// Cria as tabs com o mesmo adapter utilizado pelo ViewPager
+        tabLayout.setupWithViewPager(viewPager);
+        int cor = ContextCompat.getColor(getContext(), R.color.white);
+// Cor branca no texto (o fundo azul foi definido no layout)
+        tabLayout.setTabTextColors(cor, cor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
