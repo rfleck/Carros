6287bd71ce99b9bc77526f8917aca7162fa09a6e
Roberson Fleck <fleckpower@gmail.com>
2016-03-19 10:28:51 -0300

    OK

---
+++ Carros/app/src/main/java/br/com/livroandroid/carros/activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package br.com.livroandroid.carros.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rfleck on 3/19/16.
+ */
+public class BaseActivity extends livroandroid.lib.activity.BaseActivity {
+}
---
+++ Carros/app/src/main/java/br/com/livroandroid/carros/fragments/BaseFragment.java
@@ -0,0 +1,11 @@
+package br.com.livroandroid.carros.fragments;
+
+// import android.app.Fragment;
+// Adjust to use legacy/compatible library instead of the latest suggested by Studio
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by rfleck on 3/19/16.
+ */
+public class BaseFragment extends livroandroid.lib.fragment.BaseFragment {
+}
