17dc0354dd2d9b5f64495aa0178333b29d975fa4
Roberson Fleck <fleckpower@gmail.com>
2016-03-19 09:34:21 -0300

    Initial commit

---
+++ LoginTestApp/app/src/main/java/br/com/up/rfleck/logintestapp/Main.java
@@ -0,0 +1,53 @@
+package br.com.up.rfleck.logintestapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView tLogin = (TextView) findViewById(R.id.tLogin);
+        TextView tSenha = (TextView) findViewById(R.id.tSenha);
+
+        String login = tLogin.getText().toString();
+        String senha = tLogin.getText().toString();
+
+    }
+}
